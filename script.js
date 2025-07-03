0cd89d1e21ccea8af104e8591b8fc3e053e330f3
RAFAELLA REDER SANTOS TAVEIRA <rafaellataveirapay@gmail.com>
2025-07-03 15:29:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const nomes = new Set();
 
 function adicionar  () {
     const nome = document.getElementById ("nome");
+    let nomeLimpo = nome.value.toUpperCase();
+    nomeLimpo = nomeLimpo.trim();
+    if (!nomes.has(nomeLimpo)  && (nomeLimpo== " ")) {
+
     const lista = document.getElementsByTagName ("ul") [0]
     const item = document.createElement("li");
     const nomeUpper = nome.value.toUpperCase();
     console.log(nomeUpper)
-    item.textContent=nomeUpper;
-    nomes.add(nome.value)
+    item.textContent=nomeLimpo;
+    nomes.add(nomeLimpo)
     console.log(nomes)
     lista.appendChild(item)
     nome.value=" ";
 
+}
 }
 
 function sortear () {
@@ -22,4 +27,5 @@ function sortear () {
     const itemSorteado = document.createElement("li")
     itemSorteado.textContent=sorteado;
     listaSorteados.appendChild(itemSorteado);
+    
 }
